1b8744d7b1a03fbfa7b9299ba9893af72eb82fd2
Breanna Schneider <bjschneider4@wisc.edu>
2024-04-21 12:49:20 -0500

    updated

--- process.sh
+++ process.sh
@@ -7,5 +7,15 @@ cat itineraries.csv  | tail -n +2 | cut -d ',' -f2,3,4,5,7,14,15,27,23,13 | awk 
  
 echo "Done" 
 
+# untar your R installation
+tar -xzf R413.tar.gz
+
+# make sure the script will use your R installation, 
+# and the working directory as its home location
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
+
+
 
 Rscript process.R $1
